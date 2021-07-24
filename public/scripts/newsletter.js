a23d6e460e8983260bfa3011e2635f9ced562708
kryuki <kobayashiryuki1002@gmail.com>
2021-07-23 23:58:42 -0500

    create delete function

--- public/scripts/newsletter.js
+++ public/scripts/newsletter.js
@@ -1,1 +1,11 @@
 // JS file for newsletter / files page
+//Set delete function
+document.querySelectorAll(".deleteBtn").forEach((element, index) => {
+    element.addEventListener('click', function() {
+        $.ajax({
+            type: 'DELETE',
+            url: `/portal/newsletter/delete/${index}`,
+            contentType: 'application/json'
+        })
+    });
+})
