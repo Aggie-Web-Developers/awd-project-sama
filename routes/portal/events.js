6097243962995272d59cb16a88cacd94b9d1d555
rmatsuda <reo.matsuda@gmail.com>
2021-09-09 20:56:31 -0500

    Fixed sql security issue and added event-type creation

--- routes/portal/events.js
+++ routes/portal/events.js
@@ -76,16 +76,18 @@ router.post(
 	'/meeting/create',
 	middleware.checkAuthenticated,
 	async (req, res) => {
-		const name = req.body.meetingName;
-		const description = req.body.meetingDescription;
-		const link = req.body.meetingLink;
-		const time = req.body.meetingTime;
-		const day = req.body.meetingDay;
+		const sqlReq = new sql.Request();
+
+		sqlReq.input('name', sql.NVarChar, req.body.meetingName);
+		sqlReq.input('description', sql.NVarChar, req.body.meetingDescription);
+		sqlReq.input('link', sql.NVarChar, req.body.meetingLink);
+		sqlReq.input('time', sql.NVarChar, req.body.meetingTime);
+		sqlReq.input('day', sql.NVarChar, req.body.meetingDay);
 
 		var sqlQuery = `INSERT INTO tbl_meeting(name, description, link, time, day) 
-				VALUES ('${name}', '${description}', '${link}', '${time}', '${day}')`;
+				VALUES (@name, @description, @link, @time, @day)`;
 
-		var sqlReq = new sql.Request()
+		sqlReq
 			.query(sqlQuery)
 			.then((result) => {
 				getMeeting(req, res);
@@ -119,18 +121,20 @@ router.post('/meeting/update/:id', middleware.checkAuthenticated, function (
 	req,
 	res
 ) {
-	const name = req.body.meetingName;
-	const description = req.body.meetingDescription;
-	const link = req.body.meetingLink;
-	const time = req.body.meetingTime;
-	const day = req.body.meetingDay;
+	const sqlReq = new sql.Request();
 
-	var sqlQuery = `UPDATE tbl_meeting SET name = '${name}', description = '${description}', link = '${link}', time = '${time}', day = '${day}'
+	sqlReq.input('name', sql.NVarChar, req.body.meetingName);
+	sqlReq.input('description', sql.NVarChar, req.body.meetingDescription);
+	sqlReq.input('link', sql.NVarChar, req.body.meetingLink);
+	sqlReq.input('time', sql.NVarChar, req.body.meetingTime);
+	sqlReq.input('day', sql.NVarChar, req.body.meetingDay);
+
+	var sqlQuery = `UPDATE tbl_meeting SET name = @name, description = @description, link = @link, time = @time, day = @day
 				WHERE id=${req.params['id']}`;
 
 	console.log(sqlQuery);
 
-	var sqlReq = new sql.Request()
+	sqlReq
 		.query(sqlQuery)
 		.then((result) => {
 			getMeeting(req, res);
@@ -219,17 +223,17 @@ router.post(
 	'/eventType/update/:id',
 	middleware.checkAuthenticated,
 	async function (req, res) {
-		console.log(req.body.events);
-
 		var updateEvents = [];
 		req.body.events.forEach((event) => {
-			console.log(event);
 			updateEvents.push(updateEvent(event.name, event.id));
 		});
 		await Promise.all(updateEvents);
+		const sqlReq = new sql.Request();
 
-		var sqlQuery = `UPDATE tbl_event_type SET name='${req.body.eventTypeName}' WHERE id=${req.params['id']}`;
-		await new sql.Request()
+		sqlReq.input('name', sql.NVarChar, req.body.eventTypeName);
+
+		var sqlQuery = `UPDATE tbl_event_type SET name = @name WHERE id=${req.params['id']}`;
+		sqlReq
 			.query(sqlQuery)
 			.then((result) => {
 				getMeeting(req, res);
@@ -289,6 +293,88 @@ router.post('/event/delete/:id', middleware.checkAuthenticated, function (
 			res.send('Failed to delete');
 		});
 });
+
+async function createEvent(name, id) {
+	var sqlQuery = `INSERT INTO tbl_event(name, event_type_id) VALUES ('${name}', ${id});`;
+	console.log(sqlQuery);
+
+	return new Promise(function (resolve, reject) {
+		new sql.Request()
+			.query(sqlQuery)
+			.then((result) => {
+				resolve(result);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	}).catch((err) => {
+		console.log(err);
+		console.log('SSQS');
+	});
+}
+
+router.get('/eventType/create', middleware.checkAuthenticated, async function (req,res) {
+	var sqlQuery = 'SELECT MAX(id) FROM tbl_event_type';
+	var maxID = 0;
+	await new sql.Request()
+		.query(sqlQuery)
+		.then((result) => {
+			if(result.recordset[0][''] === null) 
+				maxID = 0;
+			else
+				maxID = result.recordset[0][''] + 1;
+		})
+		.catch((err) => {
+			res.status(400);
+			res.send('Error!');
+		});
+
+	res.render('create-eventType',  {id: maxID});
+});
+
+router.post(
+	'/eventType/create',
+	middleware.checkAuthenticated,
+	async function (req, res) {
+		var sqlQuery = 'SELECT MAX(id) FROM tbl_event_type';
+		var maxID = 0;
+		await new sql.Request()
+			.query(sqlQuery)
+			.then((result) => {
+				if(result.recordset[0][''] === null) 
+					maxID = 0;
+				else
+					maxID = result.recordset[0][''] + 1;
+			})
+			.catch((err) => {
+				res.status(400);
+				res.send('Error!');
+			});
+		
+		const sqlReq = new sql.Request();
+
+		sqlReq.input('name', sql.NVarChar, req.body.eventTypeName);
+		var sqlQuery = `INSERT INTO tbl_event_type(id, name, img) VALUES (${maxID},@name,'')`;
+		sqlReq
+			.query(sqlQuery)
+			.then(async (result) => {
+				var updateEvents = [];
+				if(req.body.events !== undefined) {
+					req.body.events.forEach((event) => {
+						updateEvents.push(createEvent(event.name, maxID));
+					});
+					await Promise.all(updateEvents);
+				}
+				getMeeting(req, res);
+			})
+			.catch((err) => {
+				console.log(err);
+				req.flash('error', 'Error updating event type');
+			});
+		
+
+	}
+);
 
 router.get('/eventType/delete/:id', middleware.checkAuthenticated, function (
 	req,
